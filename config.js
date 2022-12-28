1fc703c1db821c8c7f85f6e52efad6535a84afdc
ren-kunnnnn <121303512+ren-kunnnnn@users.noreply.github.com>
2022-12-28 09:51:26 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introTitle: 'Daniseee',
     introDesc: `What if gala us, treat ko na :>`,
     btnIntro: 'AYOKO',
-    title: 'Anong ayoko, di ako payag (¬_¬")',
+    title: 'Anong ayoko, di ako payag! (¬_¬")',
     desc: 'One more time (っ˘̩╭╮˘̩)っ ',
     btnYes: 'Sige na nga',
     btnNo: 'Ayoko talaga',
     question: 'So When ka free HAHAHA',
     btnReply: 'Send',
-    reply: 'Chat mo ko ako na mag-set HAHAHAHAHA',
+    reply: 'Free ako basta ikaw... YIEEEEEE',
     mess: 'SM Cab?',
-    messDesc: 'Ikaw na bahala sa time.',
+    messDesc: 'Ikaw na bahala sa time ʕ •ᴥ•ʔ',
     btnAccept: 'Satchel out!',   
     messLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'
 }
